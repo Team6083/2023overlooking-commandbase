44d1962604229d968647d3e46145ea8dbaec09d6
jeremy1214 <s1110280@my.cmsh.cyc.edu.tw>
2023-08-21 16:03:16 +0800

    [add] pathing

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,12 +1,14 @@
 package frc.robot;
 
+import edu.wpi.first.math.trajectory.Trajectory;
+
 public final class Constants {
-    public static class XboxControllerPort{
+    public static class XboxControllerPort {
         public static final int kmain = 0;
         public static final int kvice = 1;
     }
-    
-    public static class DrivebaseSubConstants{
+
+    public static class DrivebaseSubConstants {
         public static final double kTrackWidthMeters = 0.53;
         public static final double ks = 0.63228;
         public static final double kv = 2.1943;
@@ -37,5 +39,34 @@ public final class Constants {
         // pid
         public static final double kP = 0.3;
         public static final double[][] armAngleSetpoints = { { 35.6, 28.38, 90, -10 }, { 130, 151, 90, 180 } };
+    }
+
+    public static class AutoEnginePath {
+        public static final int trajectoryAmount = 6;
+        public static final int[] blueLeft = { 0 };
+        public static final int[] blueMiddle = { 1 };
+        public static final int[] blueRight = { 2 };
+        public static final int[] redLeft = { 3 };
+        public static final int[] redMiddle = { 4 };
+        public static final int[] redRight = { 5 };
+        public static final String[] trajJSON = {
+                "/home/lvuser/deploy/output/output/BL.wpilib.json", "/home/lvuser/deploy/output/output/BM.wpilib.json",
+                "/home/lvuser/deploy/output/output/BR.wpilib.json", "/home/lvuser/deploy/output/output/RL.wpilib.json",
+                "/home/lvuser/deploy/output/output/RM.wpilib.json", "/home/lvuser/deploy/output/output/RR.wpilib.json"
+        };
+
+        public static final Trajectory[] trajectory = new Trajectory[trajectoryAmount];
+        public static final String DoNothing = "DoNothing";
+        public static final String BlueLeft = "BlueLeft";
+        public static final String BlueMiddle = "BlueMiddle";
+        public static final String BlueRight = "BlueRight";
+        public static final String RedLeft = "RedLeft";
+        public static final String RedMiddle = "RedMiddle";
+        public static final String RedRight = "RedRight";
+
+        // The string of the timer
+        public static final String SideTimer = "SideTimer";
+        public static final String MiddleTimer = "MiddleTimer";
+        public static final String GoBackTimer = "GoBackTimer";
     }
 }
