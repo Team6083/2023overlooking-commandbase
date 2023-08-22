712a57acccea99a72aad4ab38c37d10f574f7cf7
William960321 <s1110270@my.cmsh.cyc.edu.tw>
2023-08-22 13:23:30 +0800

    [add] func

--- src/main/java/frc/robot/Subsystem/ArmSystem/ArmSubsystem.java
+++ src/main/java/frc/robot/Subsystem/ArmSystem/ArmSubsystem.java
@@ -4,14 +4,61 @@
 
 package frc.robot.Subsystem.ArmSystem;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Subsystem.ArmSystem.JointSubsystem;
+import frc.robot.Constants.ArmSubConstants;
 
 public class ArmSubsystem extends SubsystemBase {
+  protected JointSubsystem joint;
+  protected LineSubsystem line;
+
   /** Creates a new ArmSubsystem. */
-  public ArmSubsystem() {}
+  public ArmSubsystem() {
+    joint = new JointSubsystem();
+    line = new LineSubsystem();
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    putDashboard();
+    joint.pidControlLoop();
+    // line.pidControlLoop();
+  }
+
+  public double getAngleDegree() {
+    return joint.getAngleDegree();
+  }
+
+  // public double getLength() {
+  // return line.getLineLength();
+  // }
+
+  public void setAngleSetPoint(double angleSetPoint) {
+    joint.setSetpoint(angleSetPoint);
+    return;
+  }
+
+  // public void setLineSetPoint(double lineSetPoint) {
+  // line.setPIDSetpoint(lineSetPoint);
+  // return;
+  // }
+
+  public double getLineMaxLengthByJointAngle(double jointAngleRadian) {
+    double angle = jointAngleRadian;
+
+    double maxProj = ArmSubConstants.jointToFrameDist + ArmSubConstants.extendLimit;
+    double maxLenByExtendLimit = (maxProj / Math.abs(Math.cos(angle))) - ArmSubConstants.firstStageToJoint;
+
+    double maxLenByHeightLimit = ((ArmSubConstants.heightLimit - ArmSubConstants.jointHeight)
+        / Math.abs(Math.sin(angle))) - ArmSubConstants.firstStageToJoint;
+
+    return Math.min(maxLenByExtendLimit, maxLenByHeightLimit);
+  }
+
+  public void putDashboard() {
+    SmartDashboard.putNumber("joint_angle", getAngleDegree());
+    // SmartDashboard.putNumber("line_length", getLength());
   }
 }
