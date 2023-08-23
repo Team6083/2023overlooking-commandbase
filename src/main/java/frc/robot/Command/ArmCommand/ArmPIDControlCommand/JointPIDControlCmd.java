e7543a7a085b8a9bdf12665056b439beeed13f7e
William960321 <s1110270@my.cmsh.cyc.edu.tw>
2023-08-23 16:21:33 +0800

    [add] pidControlLoop

--- src/main/java/frc/robot/Command/ArmCommand/ArmPIDControlCommand/JointPIDControlCmd.java
+++ src/main/java/frc/robot/Command/ArmCommand/ArmPIDControlCommand/JointPIDControlCmd.java
@@ -9,10 +9,15 @@ import frc.robot.Subsystem.ArmSystem.ArmSubsystem;
 
 public class JointPIDControlCmd extends CommandBase {
   private final ArmSubsystem armSubsystem;
+  private double leftTriggerValue;
+  private double rightTriggerValue;
+  private double armAngleModify;
 
   /** Creates a new ArmPIDControlCmd. */
-  public JointPIDControlCmd(ArmSubsystem m_ArmSubsystem) {
+  public JointPIDControlCmd(ArmSubsystem m_ArmSubsystem, double m_m_leftTrigger, double m_m_rightTrigger) {
     this.armSubsystem = m_ArmSubsystem;
+    this.leftTriggerValue = m_m_leftTrigger;
+    this.rightTriggerValue = m_m_rightTrigger;
 
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(armSubsystem);
@@ -27,6 +32,8 @@ public class JointPIDControlCmd extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    armAngleModify = (leftTriggerValue - rightTriggerValue) * -0.7;
+    armSubsystem.setAngleSetpoint(armSubsystem.getJointSetpoint() + armAngleModify);
     armSubsystem.jointPIDControlLoop();
   }
 
