9f7513cece2b313c6d166f8508ce040d089d218d
jeremy1214 <s1110280@my.cmsh.cyc.edu.tw>
2023-09-02 01:28:23 +0800

    [add] the folder name of autoenginesmallpath

--- src/main/java/frc/robot/Command/AutoModeCmdGroup/AutoEngineBackward.java
+++ src/main/java/frc/robot/Command/AutoModeCmdGroup/AutoEngineBackward.java
@@ -2,16 +2,16 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.Command.AutoMode;
+package frc.robot.Command.AutoModeCmdGroup;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class PathOne extends SequentialCommandGroup {
-  /** Creates a new PathOne. */
-  public PathOne() {
+public class AutoEngineBackward extends SequentialCommandGroup {
+  /** Creates a new AutoEngineBackward. */
+  public AutoEngineBackward() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands();
