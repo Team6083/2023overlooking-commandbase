6d37767661a564f92604a49cd3abc1174b272518
jeremy1214 <s1110280@my.cmsh.cyc.edu.tw>
2023-09-02 01:24:43 +0800

    [mod]change empty file to sequential command group

--- src/main/java/frc/robot/Command/DrivebaseCommand/AutoDrive/AutoEngineSmallPath/AutoEngineBackward.java
+++ src/main/java/frc/robot/Command/DrivebaseCommand/AutoDrive/AutoEngineSmallPath/AutoEngineBackward.java
@@ -9,9 +9,9 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class AutoEngineRightCmd extends SequentialCommandGroup {
-  /** Creates a new AutoEngineRightCmd. */
-  public AutoEngineRightCmd() {
+public class AutoEngineBackward extends SequentialCommandGroup {
+  /** Creates a new AutoEngineBackward. */
+  public AutoEngineBackward() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands();
